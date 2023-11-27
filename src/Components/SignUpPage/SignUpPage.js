89b7037e268360573986e34ef17d7744eb3648e3
ArtKrutsko <124406508+ArtKrutsko@users.noreply.github.com>
2023-11-26 19:46:44 -0500

    password hash

--- src/Components/SignUpPage/SignUpPage.js
+++ src/Components/SignUpPage/SignUpPage.js
@@ -6,15 +6,55 @@ import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
 
 export const SignUpPage = () => {
     const history = useHistory();
+    var submit = "";
+    const bcrypt = require('bcrypt');
 
+    function setStudent() {
+      submit = "student";
+    }
+    
+    function setTutor() {
+      submit = "tutor";
+    }
 
     function handleSubmit (event) {
+      event.preventDefault();
+
+      if (submit ==="tutor")
+        handleSubmitTutor(event);
+      else
+        handleSubmitStudent(event);
+    }
+
+
+    function hash_password(userPassword) {
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(userPassword, salt, (err, hash) => {
+          return hash;
+        });
+      });
+    }
+
+    function handleSubmitTutor (event) {
+      event.preventDefault();
+
+      fetch('https://localhost:7185/api/users', {
+        method: 'POST',
+        headers: {"Content-Type":'application/json'},
+        body: JSON.stringify({id:0, name:event.target.fname.value, surname: event.target.lname.value, email: event.target.email.value, phoneNumber: event.target.phone.value, password:hash_password(event.target.password.value), dateOfBirth:"1980-01-02T00:00:00", rating:0.0,imagePath:"default"}),
+      }).then(response => response.json()).then(responseJson =>  {    
+          history.push("/");
+      });
+  }
+
+
+    function handleSubmitStudent (event) {
         event.preventDefault();
 
         fetch('https://localhost:7185/api/users', {
           method: 'POST',
           headers: {"Content-Type":'application/json'},
-          body: JSON.stringify({id:0, name:event.target.fname.value, surname: event.target.lname.value, email: event.target.email.value, phoneNumber: event.target.phone.value, password:event.target.password.value, dateOfBirth:"1980-01-02T00:00:00", rating:0.0,imagePath:"default"}),
+          body: JSON.stringify({id:0, name:event.target.fname.value, surname: event.target.lname.value, email: event.target.email.value, phoneNumber: event.target.phone.value, password:hash_password(event.target.password.value), dateOfBirth:"1980-01-02T00:00:00", rating:0.0,imagePath:"default"}),
         }).then(response => response.json()).then(responseJson =>  {    
             history.push("/");
         });
@@ -27,7 +67,7 @@ export const SignUpPage = () => {
           <div className='logo-image'>
             <img src={logo} alt='Find-a-Bull logo'/>
           </div>
-          <form onSubmit={handleSubmit} method='POST' action='http://localhost:5000/users/'>
+          <form onSubmit={handleSubmit}>
 
             <div className='item signup username'>
                 <h2>First name:</h2>
@@ -54,10 +94,10 @@ export const SignUpPage = () => {
                 <input placeholder='your password' name="password" type='password' required></input>
             </div>
             <div className='login'>
-                <button type='submit' className="submit-button">Sign Up as Student</button>
+                <button type='submit' className="submit-button" onClick={setStudent}>Sign Up as Student</button>
             </div>
             <div className='login sign'>
-                <button type='submit' className="submit-button">Sign Up as Tutor</button>
+                <button type='submit' className="submit-button" onClick={setTutor}>Sign Up as Tutor</button>
             </div>
             <NavLink to='/'>
               <div className='login sign'>
